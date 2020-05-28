4e38c0976e364e8e15e1c5dad1b3a7059f18e297
Hopewow <43467655+Hopewow@users.noreply.github.com>
2020-05-28 11:21:58 +0200

    Add project files.

---
+++ GameChanger Projekt - El Ti/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameChanger_Projekt___El_Ti.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
